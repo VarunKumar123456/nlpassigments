8e203a9959eb4bb9de7a92080295f5d0e498dd18
VarunKumar123456 <157882545+VarunKumar123456@users.noreply.github.com>
2025-01-07 12:51:03 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "2d5d8e8f-6c79-4083-82ab-673000c45e1b",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
